aef278028832ed3fc52ab6dfdb6b53a92a330f9b
nikeshsingh1947 <141706194+nikeshsingh1947@users.noreply.github.com>
2023-10-12 11:18:21 +0000

    Build: (905a36e) cms

--- src/components/Boxcard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Boxcard(props) {
-    return (
-      <div style={{ background: props.color, width: "100%", height: "100%" }} />
-    );
-  }
-  export default Boxcard;
